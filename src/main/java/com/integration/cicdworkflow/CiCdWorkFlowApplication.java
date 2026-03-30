90d362289717fd31b4853c098cff5a1da9f6b3c8
Kroem Vandy <kroem123vandy@gmail.com>
2026-03-30 21:08:38 +0700

    first commit no code

---
+++ src/main/java/com/integration/cicdworkflow/CiCdWorkFlowApplication.java
@@ -0,0 +1,13 @@
+package com.integration.cicdworkflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdWorkFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiCdWorkFlowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/integration/cicdworkflow/CiCdWorkFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.integration.cicdworkflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdWorkFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
